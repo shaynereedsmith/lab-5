13762e759dde63b712b63b388d95f905a702b4f7
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-11 15:31:48 -0700

    problem 3, adding variables and concatenation. Hope to have the problem solved soon.

--- app.js
+++ app.js
@@ -47,13 +47,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-  var sum = '';
-  var product = '';
-  return [];
+  var element1 = sum(sum(a,b),c);
+  var element2 = multiply(multiply(a,b),c);
+  return [element1, element2];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
