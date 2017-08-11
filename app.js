95b45a9c11bd6c18b92e678e2cf8b34da7448293
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-11 16:54:38 -0700

    first commit for problem 4

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ function sumArray(testArray){ //eslint-disable-line
 }
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
